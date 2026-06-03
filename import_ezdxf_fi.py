fe234d17874d8c32a99443e03d03ffc39e3b9f94
SARA SAKEENA <143297666+sarasakeena@users.noreply.github.com>
2026-06-03 18:09:53 +0530

    Fix typo in TARGET_LAYER name

--- import_ezdxf_fi.py
+++ import_ezdxf_fi.py
@@ -26,7 +26,7 @@ def update_globals(params):
     _spacing_text_drawn.clear()
 
 
-TARGET_LAYER = "RCC_BEMS"
+TARGET_LAYER = "RCC_BEAMS"
 GENERATED_LAYER = "GENERATED_PERP_LINES"
 BEHIND_MIN_DISTANCE = 10
 BEHIND_MAX_DISTANCE = 450
